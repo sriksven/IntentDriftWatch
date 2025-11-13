06d88443f8557e265030f3874b0aa948e16fe333
Krishna Venkatesh <43298886+sriksven@users.noreply.github.com>
2025-11-12 19:40:31 -0500

    added cicd

--- intentdriftwatch-ui/vite.config.js
+++ intentdriftwatch-ui/vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/intentdriftwatch/", // 👈 repository name here
+  base: "/IntentDriftWatch/", // 👈 repository name here
 });
 
